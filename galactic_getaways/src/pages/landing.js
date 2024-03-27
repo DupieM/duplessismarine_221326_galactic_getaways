e1571e604c0611b71c1dde85143ad378f4124ead
DupieM <221326@virtualwindow.co.za>
2024-03-27 12:32:06 +0200

    Landing Page Frontend Part 3

--- galactic_getaways/src/pages/landing.js
+++ galactic_getaways/src/pages/landing.js
@@ -2,7 +2,8 @@ import '../App.css';
 import './landing.css';
 import React from "react";
 import img from  '../Images/Header_background.jpg';
-import img2 from  '../Images/mobile_app_background.jpg'
+import img2 from  '../Images/mobile_app_background.jpg';
+import img3 from '../Images/Apple_iPhone_15_Pro_White_Titanium_1.png'
 
 function Landing(){
     return (
@@ -30,18 +31,34 @@ function Landing(){
             <div className='box2'>
                 <h2 className="Sub_Heading_2">A Stellar Selection</h2>
                 <p className="Body_2">
-                    Explore three breathtaking destinations, each with its own unique charm and mystery.
+                    Explore three breathtaking destinations, each with <br/>
+                    its own unique charm and mystery.
                 </p>
+                <div class="container">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-3">
+                            <div class="p-3 border bg-light">Custom column padding</div>
+                        </div>
+                        <div class="col-3">
+                            <div class="p-3 border bg-light">Custom column padding</div>
+                        </div>
+                        <div class="col-3">
+                            <div class="p-3 border bg-light">Custom column padding</div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div>
+            <div className='box3'>
                 <img src={img2} className='bg_img2'/>
-                <div>
+                <div className="maincard2">
                     <h2 className="Sub_Heading_3">THE STARS IN YOUR POCKET... Coming soon</h2>
                     <p className="Body_3">
-                        We are very excited to launch our very own Celestial Voyages mobile application 
-                        coming soon to the App Store & Google Play Store.
+                        We are very excited to launch our very own <br/>
+                        Celestial Voyages mobile application coming soon <br/>
+                        to the App Store & Google Play Store.
                     </p>
+                    <img src={img3} className='bg_img3'/>
                 </div>
             </div>
 
